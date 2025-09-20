c8919408f4951de527e0f6dbbedc30f761efb25a
Thomas Kong <txdk2009@gmail.com>
2025-09-21 00:05:58 +0930

    Add cursor styling

--- init.lua
+++ init.lua
@@ -91,6 +91,10 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
+-- set cursor
+vim.o.guicursor =
+  'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
+
 -- tab settings
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
